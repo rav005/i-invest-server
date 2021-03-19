6aab00400e9934fe1f73597ad0128b6927f97c6e
Gaurav <gauravpatel@hotmail.ca>
2021-03-18 23:32:41 -0400

    watchlist

--- controllers/api.js
+++ controllers/api.js
@@ -19,10 +19,15 @@ function getApiObject() {
 }
 
 async function getStockCurrentRate(stockSymbol) {
-    await getApiObject().quote(stockSymbol, (error, data, response) => {
-        console.log("getStockCurrentRate: ", data);
-        return data;
+    getApiObject().quote(stockSymbol).then(res => {
+        console.log('getStockCurrentRate: res => ', err);
+    }).catch(err => {
+        console.log('getStockCurrentRate: err =>', err);
     });
+    // await getApiObject().quote(stockSymbol, (error, data, response) => {
+    //     console.log("getStockCurrentRate: ", data);
+    //     return data;
+    // });
 }
 
 async function getRateForWatchList(watchList) {
@@ -30,9 +35,25 @@ async function getRateForWatchList(watchList) {
         return;
     }
 
-    watchList.forEach(stock => {
-        console.log(await getStockCurrentRate(stock.symbol));
+    Promise.all(watchList.map(async (stock) => {
+        return new Promise((resolve, reject) => {
+            getApiObject().quote(stock.symbol, (error, data, response) => {
+                if (error) {
+                    return reject(error);
+                }
+                var s = {name: stock.name, symbol: stock.symbol, data: data};
+                return resolve(s);
+            });
+        })
+    })).then(resp => {
+        return resp;
+    }).catch(err => {
+        console.log('err => ', err.message);
     });
+
+    // watchList.forEach(stock => {
+    //     console.log( getStockCurrentRate(stock.symbol));
+    // });
 
     // await Promise.all(watchList.map(async (stock) => {
 
