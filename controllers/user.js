529a77db31d3b1ffab4a329950385cccf0cf71c2
nabyl <9850615+nabilv@users.noreply.github.com>
2021-04-04 19:19:38 -0400

    return 400 when signing up with existing username

--- controllers/user.js
+++ controllers/user.js
@@ -6,24 +6,16 @@ const db = require('../services/db');
 const api = require('./api');
 
 router.post('/login', async (req, resp) => {
-
-    //console.log("/login ", req.body);
-
     const username = req.body.username;
     const password = req.body.password;
     const user = await common.findUserByUsername(username);
-    //console.log(user);
     if (user && password) {
         user.comparePassword(password, async (err, isMatch) => {
             if (err || !isMatch) {
                 resp.status(403).send();
                 return;
             }
-            //console.log('valid password:', isMatch);
             const token = common.generateAccessToken({ id: user._id });
-            //console.log(user.watchList);
-            //var watchList = await api.getRateForWatchList(user.watchList);
-            //console.log("==>", watchList);
             resp.status(200).json({ token: token });
         });
     }
@@ -35,7 +27,6 @@ router.post('/login', async (req, resp) => {
 router.post('/passwordreset', async (req, resp) => {
     const username = req.body.username;
     const user = await common.findUserByUsername(username);
-    //console.log(user);
     if (user) {
         resp.status(200).json({ "question": user.question });
     }
@@ -47,7 +38,6 @@ router.post('/passwordreset', async (req, resp) => {
 router.post('/passwordresetquestion', async (req, resp) => {
     const username = req.body.username;
     const user = await common.findUserByUsername(username);
-    //console.log(user);
     if (user) {
 
         user.compareAnswer(req.body.answer, (err, isMatch) => {
@@ -55,7 +45,6 @@ router.post('/passwordresetquestion', async (req, resp) => {
                 resp.status(403).send();
                 return;
             }
-            //console.log('valid answer:', isMatch);
             const token = common.generateAccessToken({ id: user._id }, '120s');
             resp.status(200).json({ token: token });
         });
@@ -70,7 +59,6 @@ router.post('/passwordchange', async (req, resp) => {
     const token = req.body.token;
     const newPasswordHash = await User.getHash(newPassword);
     if (token && newPassword && newPasswordHash) {
-        //console.log("newpasswordhash: ", newPasswordHash);
         const id = common.verifyJwt(token);
         if (id) {
             db.connect();
@@ -88,20 +76,27 @@ router.post('/passwordchange', async (req, resp) => {
 })
 
 router.post('/signup', async (req, resp) => {
-    //console.log('req body: ', req.body);
     const user = new User(req.body);
 
     db.connect();
 
-    user.save(error => {
-        if (common.checkServerError(resp, error)) {
-            console.log("/user/signup err: ", error, ", req: ", req.body);
-            return;
-        }
-        const token = common.generateAccessToken({ id: user._id });
-        resp.status(201).json({ token: token });
-        console.log('user created successfully!');
-    });
+    const userInDb = await common.findUserByUsername(user.username);
+    if (userInDb) {
+        const msg = "Username already exists";
+        common.log("", "/user/signup err", msg);
+        resp.status(400).json({ msg: msg });
+    }
+    else {
+        user.save(error => {
+            if (common.checkServerError(resp, error)) {
+                console.log("/user/signup err: ", error, ", req: ", req.body);
+                return;
+            }
+            const token = common.generateAccessToken({ id: user._id });
+            resp.status(201).json({ token: token });
+            console.log('user created successfully!');
+        });
+    }
 });
 
 module.exports = router;
